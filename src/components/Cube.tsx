43b2ac2eac9d61f4ae2993ed662700c661512166
Alex Welcing <alexwelcing@gmail.com>
2023-06-12 14:03:06 +0000

    yellow

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -6,7 +6,7 @@ type CubeType = Mesh<BoxGeometry, MeshBasicMaterial>
 const Cube = forwardRef<CubeType>((_, ref) => (
   <mesh ref={ref} position-x={2} castShadow>
     <boxGeometry args={[1.5, 1.5, 1.5]} />
-    <meshStandardMaterial color={'mediumpurple'} />
+    <meshStandardMaterial color={'yellow'} />
   </mesh>
 ))
 
